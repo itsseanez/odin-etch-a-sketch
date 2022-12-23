a9479f4be324f67a91b93d13154523228635c5f0
Sean Ezenwa <seanezenwa@gmail.com>
2022-12-23 13:21:09 -0500

    Start grid logic and add flexbox to grid

--- logic.js
+++ logic.js
@@ -5,3 +5,18 @@ let gridArray= new Array(rows);
 for(let i=0; i < rows; i++) {
     gridArray[i] = new Array(cols);
 }
+
+//Create grid on page
+function gridPage(gridArray) {
+    for(let i=0; i<gridArray.length; i++) {
+        for(let j=0; j<gridArray[i].length; j++) {
+            const container= document.querySelector('#container');
+            const block= document.createElement('div')
+            block.classList.add('block');
+            block.textContent='div';
+            container.appendChild(block);
+        }
+        document.createElement('br');
+    }
+}
+gridPage(gridArray);
